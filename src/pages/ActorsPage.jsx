4cb7f456bd3e60baadcf8af4754d6f659f10e6e8
Kris <smellbow@gmail.com>
2026-07-19 10:33:45 +0100

    added chasing heads on actors because why not

--- src/pages/ActorsPage.jsx
+++ src/pages/ActorsPage.jsx
@@ -1,5 +1,6 @@
 import useDocumentTitle from '../hooks/useDocumentTitle'
 import actors from '../data/actors'
+import publicAsset from '../utils/publicAsset'
 import './ActorsPage.css'
 import ActorBrowser from '../components/actors/ActorBrowser'
 
@@ -14,7 +15,18 @@ function ActorsPage() {
             <header className="actors-header">
                 <p className="eyebrow">Editor reference</p>
 
-                <h1 id="actors-title">Actor Reference</h1>
+                <h1 id="actors-title" className="ogre-title">
+                    <span className="orbiting-head" aria-hidden="true">
+                        <img src={publicAsset('decor/ogrehead.webp')} alt="" />
+                    </span>
+                    <span
+                        className="orbiting-head orbiting-head--trailing"
+                        aria-hidden="true"
+                    >
+                        <img src={publicAsset('decor/darkwiz.webp')} alt="" />
+                    </span>
+                    Actor Reference
+                </h1>
 
                 <p>
                     Search Wizordum’s actors, learn what their parameters control,
